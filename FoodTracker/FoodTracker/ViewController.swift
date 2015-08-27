d4ab9c6773964e9c2b6f0a8ea99751cffeb9293b
Travis Emery <travis.emery@perficient.com>
2015-08-26 21:17:46 -0400

    Adding FoodTracker Project

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Travis.Emery on 8/26/15.
+//  Copyright (c) 2015 emery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
